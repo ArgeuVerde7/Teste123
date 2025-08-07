86c86392fbc0268ac5168e61a124f78c21a478f9
ArgeuVerde7 <ciclonh79@gmail.com>
2025-08-07 16:20:45 -0300

    Add files via upload

---
+++ duplicado.js
@@ -0,0 +1,13 @@
+function calculateDiscount(price, discount) {
+  // O SonarQube pode identificar que 'discount' não é validado
+  if (discount > 100) {
+    return price; // Exemplo de um bug potencial
+  }
+  return price - (price * discount) / 100;
+}
+
+const finalPrice = calculateDiscount(100, 20);
+console.log(`O preço final é: ${finalPrice}`);
+
+// Variável não utilizada, SonarQube pode identificar isso
+const unusedVariable = "Hello World";
